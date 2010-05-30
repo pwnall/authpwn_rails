a0a644fee595d3af52389dcac03d31395ade78e7
Victor Costan <costan@gmail.com>
2010-05-30 04:18:57 -0400

    Helper exposing session info to the views.

---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,4 @@
+module SessionHelper
+  # The user currently logged in. Nil if no user is logged in.
+  attr_reader :current_user
+end
--- lib/mini_auth_rails.rb
+++ lib/mini_auth_rails.rb
@@ -7,4 +7,9 @@ require 'mini_auth_rails/session.rb'
 
 if defined?(Rails)
   require 'mini_auth_rails/engine.rb'
+
+  # HACK(costan): this works around a known Rails bug
+  #     https://rails.lighthouseapp.com/projects/8994/tickets/1905-apphelpers-within-plugin-not-being-mixed-in
+  require File.expand_path('../../app/helpers/session_helper.rb', __FILE__)
+  ActionController::Base.helper SessionHelper
 end
