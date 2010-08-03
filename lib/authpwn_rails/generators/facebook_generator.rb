b3dd0f14a65e7a2b26805f38cc6ff98098d835cb
Victor Costan <costan@gmail.com>
2010-08-03 05:00:24 -0400

    Fixed migrations path in generators.

--- lib/authpwn_rails/generators/facebook_generator.rb
+++ lib/authpwn_rails/generators/facebook_generator.rb
@@ -9,7 +9,7 @@ class FacebookGenerator < Rails::Generators::Base
     copy_file 'facebook_token.rb',
               File.join('app', 'models', 'facebook_token.rb')
     copy_file '002_create_facebook_tokens.rb',
-        File.join('db', 'migrations', '20100725000002_create_facebook_tokens.rb')
+        File.join('db', 'migrate', '20100725000002_create_facebook_tokens.rb')
     copy_file 'facebook_tokens.yml',
               File.join('test', 'fixtures', 'facebook_tokens.yml')
   end
--- lib/authpwn_rails/generators/user_generator.rb
+++ lib/authpwn_rails/generators/user_generator.rb
@@ -8,7 +8,7 @@ class UserGenerator < Rails::Generators::Base
   def create_user_model
     copy_file 'user.rb', File.join('app', 'models', 'user.rb')
     copy_file '001_create_users.rb',
-        File.join('db', 'migrations', '20100725000001_create_users.rb')
+        File.join('db', 'migrate', '20100725000001_create_users.rb')
     copy_file 'users.yml', File.join('test', 'fixtures', 'users.yml')
   end
 end  # class AuthpwnRails::UserGenerator
